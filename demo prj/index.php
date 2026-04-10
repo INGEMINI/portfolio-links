ca2f8e8ce90c575fd8d40956e4f5cb69b1717425
INGEMINI <mahiagarwal.r1@gmail.com>
2026-04-10 13:23:38 +0530

    Add files via upload

---
+++ demo prj/index.php
@@ -0,0 +1,68 @@
+<?php
+$server = "localhost";
+$username = "root";
+$password = "";
+
+$con = mysqli_connect($server, $username, $password , "trip");
+
+if(!$con){
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+// IMPORTANT CONDITION
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
+
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $gender = $_POST['gender'];
+    
+    $age = $_POST['age'];
+    $phone = $_POST['phone'];
+    $desc = $_POST['desc'];
+
+    $sql = "INSERT INTO `trip`.`trip` (`name`, `age`, `gender`, `email`, `phone`, `other`, `dt`) 
+            VALUES ('$name', '$age', '$gender', '$email','$phone','$desc', current_timestamp())";
+
+    echo $sql;
+    if($con-> query($sql) == true ){
+        echo "Successfully inserted";
+    }
+    else{
+        echo "ERROR: $sql <br> $con->error";
+    }
+
+    $con->close();
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <img src="Bugatti Symbol Wallpaper - Paperblog.jpeg" alt="bugatti symbol wallpaper" class="bg">
+  <div class = "container">
+    <h1>welcome to my project</h1>
+    <p>enter yr details and confirm yr participation in the trip </p>
+
+    <form action="index.php" method="post">
+      <input type="text" name="name" id="name" placeholder="enter your name">
+      <input type="text" name="age" id="age" placeholder="enter your age">
+      <input type="text" name="gender" id="gender" placeholder="enter your gender">
+      <input type="text" name="email" id="email" placeholder="enter your email">
+      <input type="text" name="phone" id="phone" placeholder="enter your phone number">
+      <textarea name="desc" id="desc" cols="30" rows="10" placeholder="enter any other information here"></textarea>
+
+      <button class="btn">submit</button> 
+    </form>
+
+  </div>
+  <script src="index.js"></script>
+
+
+</body>
+</html>
---
+++ demo prj/prac/basic.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<style>
+  .container {
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: #f0f0f0;
+  }
+</style>
+<body>
+  <div class="container"> this is container</div>
+</body>
+</html>
